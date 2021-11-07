ca7a28608a72781cd2ff86d8678a0173be706c36
oksanych <a.oksanych.90@gmail.com>
2021-11-07 20:33:12 +0200

    Pop-up optimisation. Fixes for form.

--- js/form.js
+++ js/form.js
@@ -1,4 +1,6 @@
 import { sendData } from './api.js';
+import{ renderPopup } from './pop-up.js';
+import { clearAll } from './map.js';
 
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
@@ -11,6 +13,8 @@ const inputTitle = document.querySelector('#title');
 const inputPrice = document.querySelector('#price');
 const inputRoomNumber = document.querySelector('#room_number');
 const inputCapacity = document.querySelector('#capacity');
+const successPopup = document.querySelector('#success').content.querySelector('.success');
+const errorPopup = document.querySelector('#error').content.querySelector('.error');
 
 // Function for disable form
 const doFormDisable = (element) => {
@@ -71,24 +75,31 @@ inputPrice.addEventListener('input', () => {
   inputPrice.reportValidity();
 });
 
-const setFormSubmit = (onSuccess, onFail, clearAll) => {
-  form.addEventListener('submit', (evt) =>{
+const setReset = () => {
+  resetBtn.addEventListener('click', (evt) => {
     evt.preventDefault();
-
-    sendData(
-      () => onSuccess(),
-      () => onFail(),
-      new FormData(evt.target),
-    );
-
     clearAll();
   });
 };
 
-const setReset = (clearAll) => {
-  resetBtn.addEventListener('click', (evt) => {
+const onSuccessPopup = () => {
+  clearAll();
+  renderPopup(successPopup);
+};
+
+const onErrorPopup = () => {
+  renderPopup(errorPopup);
+};
+
+const setFormSubmit = () => {
+  form.addEventListener('submit', (evt) =>{
     evt.preventDefault();
-    clearAll();
+
+    sendData(
+      () => onSuccessPopup(),
+      () => onErrorPopup(),
+      new FormData(evt.target),
+    );
   });
 };
 
--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-import { showAlert } from './utils.js';
-import { renderMap, clearAll } from './map.js';
 import { getData } from './api.js';
+import { showAlert } from './utils.js';
+import { renderMap } from './map.js';
 import{setFormSubmit, setReset} from './form.js';
-import{errorPopup, successPopup} from './pop-ups.js';
 
 getData(renderMap, showAlert);
 
-setReset(clearAll);
+setReset();
 
-setFormSubmit(successPopup, errorPopup, clearAll);
+setFormSubmit();
